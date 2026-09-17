f0116f2616717a9bcab8a4827a0cd71cdc0a42fd
Bereg.Levente <Bereg.Levente@C214-04>
2026-09-17 08:22:54 +0200

    Cars & Model Classes

---
+++ ConsoleApp1/Cars.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Cars
+    {
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public int BuiltYear { get; set; }
+        public int Hp { get; set; }
+        public int Price { get; set; }
+        public Cars(string line)
+        {
+            string[] data = line.Split(';');
+            Brand = data[0];
+            Type = data[1];
+            BuiltYear = int.Parse(data[2]);
+            Hp = int.Parse(data[3]);
+            Price = int.Parse(data[4]);
+        }
+    }
+}
---
+++ ConsoleApp1/Model.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Model
+    {
+        public List<Cars> cars = new List<Cars>();
+        private void Import()
+        {
+            cars = File.ReadAllLines("cars.txt").Skip(1).Select(x => new Cars(x)).ToList();
+        }
+        public Model()
+        {
+            Import();
+        }
+    }
+}
